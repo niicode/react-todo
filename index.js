7692e0112b7ad350e0f5feb86d31762729a533a3
Niidarku <niidarku66@gmail.com>
2020-11-11 13:46:24 +0000

    url added

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get('*', (req,res) =>{
 });
 
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT ||niicodetodo.netlify.app;
 app.listen(port);
 
 console.log('App is listening on port ' + port);  
